7ef8956f683b1f777fcec0d1ddd635148d5ce50b
daudroi <achmaddaud42@gmail.com>
2026-04-23 20:19:00 +0700

    menyesuaikan atribut relasi tabel kategori dan post di database

--- week-06/app/Models/Post.php
+++ week-06/app/Models/Post.php
@@ -1,29 +1,13 @@
 <?php
-
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = [
-        'title',
-        'slug',
-        'category_id',
-        'color',
-        'image',
-        'body',
-        'tags',
-        'published',
-        'published_at',
-    ];
-
-    protected $casts = [
-        'tags' => 'array',
-        'published' => 'boolean',
-        'published_at' => 'date',
-    ];
-
+    use HasFactory;
+    protected $fillable = ['title', 'slug', 'category_id', 'color', 'image', 'body', 'tags', 'published', 'published_at'];
+    protected $casts = ['tags' => 'array', 'published' => 'boolean', 'published_at' => 'date'];
     public function category()
     {
         return $this->belongsTo(Category::class);
