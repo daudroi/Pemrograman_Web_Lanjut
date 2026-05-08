9c8553ac1863f9cf2f35abd5a892914d7c05ba57
daudroi <achmaddaud42@gmail.com>
2026-05-08 07:10:23 +0700

    jobsheet 10 week 10

---
+++ week-10/app/Filament/Admin/Resources/Products/Pages/EditProduct.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Products\Pages;
+
+use App\Filament\Admin\Resources\Products\ProductResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditProduct extends EditRecord
+{
+    protected static string $resource = ProductResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            ViewAction::make(),
+            DeleteAction::make(),
+        ];
+    }
+}
---
+++ week-10/bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Providers\AppServiceProvider;
+use App\Providers\AdminPanelProvider;
+
+return [
+    AppServiceProvider::class,
+    AdminPanelProvider::class,
+];
